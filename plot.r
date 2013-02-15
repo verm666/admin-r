e3e03c2ae2ff2a0a326d47db01f6227505307101
verm666 <verm666@gmail.com>
2013-02-15 19:08:05 +0400

    Промежуточный коммит перед работой с несколькими столбцами

--- plot.r
+++ plot.r
@@ -16,25 +16,37 @@ option_list <- list(
     default="title")
 )
 
+# input file format
+#        V1      V2 ...
+# timestamp [value1 ...]
+
 o <- parse_args(OptionParser(option_list = option_list,
   usage = "%prog [options]"))
+
+if (o$scale <= 0) {
+  stop(sprintf("scale must be positive number"))
+}
 
 if (o$filename != "stdin" && file.access(o$filename, mode=4) == -1) {
   stop(sprintf("could not open file: %s", o$filename))
 }
 
-d <- data.table(read.table(o$filename, sep=" ",
-  col.names=c("timestamp", "value")))
+d <- data.table(read.table(o$filename, sep=" ", ))
+values_count = ncol(d)
 
-d$timestamp <- round(d$timestamp / o$scale) * o$scale
-d <- d[, sum(value / o$scale), by=timestamp]
+d$V1 <- round(d$V1 / o$scale) * o$scale
 
-d$timestamp <- as.POSIXct(origin="1970-01-01", d$timestamp)
+for (i in seq(from=2, to=values_count)) {
+  col_name = paste("V", i, sep="")
+  d <- d[, sum(get(col_name) / o$scale), by=V1]
+}
+
+d$V1 <- as.POSIXct(origin="1970-01-01", d$V1)
 
 X11()
 plot(d, type="l", xlab=o$xlab, ylab=o$ylab, main=o$title)
 
-write("press Ctrl+C for exit", stderr())
+write("press `ctrl+c` for exit", stderr())
 while (T) {
   Sys.sleep(10)
 }
